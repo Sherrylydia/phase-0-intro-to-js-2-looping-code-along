9924b2d60589d303a25de5904e288736e27761b0
LYDIA <lydiakinuthia91@gmail.com>
2025-03-10 17:43:02 +0300

    looing

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    // Create a new, empty array to hold the messages
+    const messages = [];
+  
+    // Iterate through the input array
+    for (let i = 0; i < names.length; i++) {
+      // Build out the thank you message using string interpolation
+      const message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+  
+      // Add the message to the new array
+      messages.push(message);
+    }
+  
+    // Return the new array
+    return messages;
+  }
+  
+  const namesArray = ["Charlie", "Samip", "Ali"];
+  const occasion = "birthday";
+  const thankYouMessages = writeCards(namesArray, occasion);
+  console.log(thankYouMessages); 
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  countDown(10);
